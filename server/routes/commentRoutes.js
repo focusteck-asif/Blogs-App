fdfe95b4d316e97dd060e55054a41ee5e60ef395
Asif <m.asif9240376@gmail.com>
2023-10-09 18:27:58 +0500

    Routes with Backend Express validations

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const commentController = require('../controllers/comment.controllers');
+const {validateCommentCreation} = require('../middleware/comment.validation.middleware')
 
-router.post('/blogs/:blogId/comments', commentController.createComment);
+router.post('/blogs/:blogId/comments', validateCommentCreation, commentController.createComment);
 
 router.get('/blogs/:blogId/comments', commentController.getComments);
 
