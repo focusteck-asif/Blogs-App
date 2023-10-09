31b09972806f36f32603aeca05d7314fd24cd9d3
Asif <m.asif9240376@gmail.com>
2023-10-09 18:24:48 +0500

    Initialization of blog columns with detailed properties

--- server/models/blog.js
+++ server/models/blog.js
@@ -17,9 +17,32 @@ module.exports = (sequelize, DataTypes) => {
   }
 
   Blog.init({
-    title: DataTypes.STRING,
-    body: DataTypes.TEXT,
-    author: DataTypes.STRING
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    author: {
+      type:  DataTypes.STRING,
+      allowNull: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
   }, {
     sequelize,
     modelName: 'Blog',
