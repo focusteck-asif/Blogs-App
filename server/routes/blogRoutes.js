ab9e6ebcb3ef803686efc8a4e59c5de38a816d84
Asif <m.asif9240376@gmail.com>
2023-10-09 18:27:46 +0500

    Routes with Backend Express validations

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const config = require('../config/jwtconfig.json')
 const { Blog } = require('../models');
 const authMiddleware = require('../middleware/authMiddleware');
 const client = require('../config/dbconnect')
 blogControllers = require('../controllers/blog.controllers')
+const {validateCreateBlog} = require('../middleware/blogs.validation.middleware')
 
 
 router.get('/blogs', authMiddleware, blogControllers.getBlogs);
@@ -15,7 +15,7 @@ router.get('/blogs/:id', blogControllers.getBlogsById);
 
 router.delete('/blogs/:id', blogControllers.deleteBlogsById)
 
-router.post('/create', authMiddleware, blogControllers.createBlog);
+router.post('/create', authMiddleware, validateCreateBlog, blogControllers.createBlog);
 
 module.exports = router
 
